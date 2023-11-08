b5e1b8245485ae19f5c576218a6420ed2358fefd
Dennis Tang <dennisvtang@gmail.com>
2023-11-07 17:57:35 -0800

    Refactor code so login credentials is reused

--- src/test.py
+++ src/test.py
@@ -1,20 +1,6 @@
-# -*- coding: utf-8 -*-
-
-# Sample Python code for youtube.liveBroadcasts.list
-# See instructions for running these code samples locally:
-# https://developers.google.com/explorer-help/code-samples#python
-
-import os
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
 from dataclasses import dataclass
-
-import google_auth_oauthlib.flow
-import googleapiclient.discovery
-import googleapiclient.errors
-
-CLIENT_SECRETS_FILE = 'token.json'
-# Disable OAuthlib's HTTPS verification when running locally.
-# *DO NOT* leave this option enabled in production.
-# os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
 @dataclass
 class LivestreamData:
@@ -22,26 +8,12 @@ class LivestreamData:
     livestream_description: str
     scheduled_start_time: str
 
-def get_livestream_data() -> LivestreamData:
-
-    # Get credentials and create an API client
-    scope = ['https://www.googleapis.com/auth/youtube.readonly']
-    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-        CLIENT_SECRETS_FILE,
-        scope,
-    )
-    credentials = flow.run_console()
-    youtube = googleapiclient.discovery.build(
-        'youtube',
-        'v3',
-        credentials=credentials
-    )
-
-    request = youtube.liveBroadcasts().list(
-        part='snippet',
-        mine=True
-    )
-    response = request.execute()
+def get_livestream_data(credentials) -> LivestreamData:
+    with build('youtube', 'v3', credentials=credentials) as service:
+        response = service.liveBroadcasts().list(
+            part='snippet',
+            mine=True,
+        ).execute()
 
     # get latest broadcast
     latest_broadcast = response['items'][0]
@@ -52,35 +24,40 @@ def get_livestream_data() -> LivestreamData:
         latest_broadcast['snippet']['publishedAt'],
     )
 
-def update_description(livestream_data: LivestreamData, text_to_append: str) -> None:
-    # Get credentials and create an API client
-    scope = ["https://www.googleapis.com/auth/youtube.force-ssl"]
-    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-        CLIENT_SECRETS_FILE,
-        scope,
-    )
-    credentials = flow.run_console()
-    youtube = googleapiclient.discovery.build(
-        'youtube',
-        'v3',
-        credentials=credentials
-    )
+def update_livestream_description(credentials, livestream_data: LivestreamData, text_to_append: str) -> None:
+    with build('youtube', 'v3', credentials=credentials) as service:
+        response = service.liveBroadcasts().update(
+            part='snippet',
+            body={
+            'id': livestream_data.livestream_id,
+            'snippet': {
+                'scheduledStartTime': livestream_data.scheduled_start_time,
+                'description': f'{livestream_data.livestream_description}\n{text_to_append}'
+            }
+            }
+        ).execute()
 
-    request = youtube.liveBroadcasts().update(
-        part='snippet',
-        body={
-          'id': livestream_data.livestream_id,
-          'snippet': {
-            'scheduledStartTime': livestream_data.scheduled_start_time,
-            'description': f'{livestream_data.livestream_description}\n{text_to_append}'
-          }
-        }
-    )
-    response = request.execute()
 
-    print(response)
+if __name__ == "__main__":
+    import os
 
-if __name__ == '__main__':
-    livestream_data = get_livestream_data()
 
-    update_description(livestream_data, '20:00 - ')
+    SCOPES = [
+        'https://www.googleapis.com/auth/youtube.readonly',
+        'https://www.googleapis.com/auth/youtube.force-ssl',
+    ]
+    if os.path.exists('token.json'):
+        flow = InstalledAppFlow.from_client_secrets_file('token.json', scopes=SCOPES)
+    else:
+        exit(
+            'Please obtain a OAuth client ID,'
+            'rename it to token.json, and add it to same folder as script,'
+            'https://github.com/googleapis/google-api-python-client/blob/main/docs/oauth-installed.md#creating-application-credentials'
+        )
+    flow.run_local_server()
+    credentials = flow.credentials
+
+
+    livestream_data = get_livestream_data(credentials)
+    print(livestream_data)
+    update_livestream_description(credentials, livestream_data, 'IT WORKED AGAIN')

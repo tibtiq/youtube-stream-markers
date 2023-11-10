9e7ae8c9f8af8030b1baa8f3e0dc667f7eac9e64
dennisvtang <dennisvtang@email.com>
2023-11-10 00:20:24 +0000

    Update from https://github.com/dennisvtang/obs-stream-markers/commit/b5147e475bdb039912a7be5a20811a48675dd337

---
+++ dep/obs-stream-markers/src/generate_stream_marker.py
@@ -0,0 +1,36 @@
+import datetime
+import time
+from typing import Union
+
+
+def get_timestamp(output_format, date_format='%Y-%m-%d %H-%M-%S'):
+    """Returns the POSIX timestamp.
+
+    Supported output formats are "float" and "string".
+    "float" returns the timestamp as a float.
+    "string" returns the timestamp in the following format: "HOUR-MINUTE-SECONDS", where zeros are
+    always padded
+    """
+    supported_formats = ['float', 'string']
+    assert output_format in supported_formats, f'Choose a supported format, {supported_formats}'
+
+    timestamp = datetime.datetime.now()
+
+    if output_format == 'float':
+        timestamp = timestamp.timestamp()
+    elif output_format == 'string':
+        timestamp = timestamp.strftime(date_format)
+
+    return timestamp
+
+
+def convert_timestamp_to_playback_time(timestamp: Union[float, int], playback_format: str = "%H:%M:%S"):
+    """Converts a timestamp (seconds) into playback time (HOUR-MINUTE-SECONDS).
+
+    The returned playback time has padded zeros.
+    """
+    assert isinstance(timestamp, (float, int)), (
+        'timestamp must be a float or int type.'
+    )
+
+    return time.strftime(playback_format, time.gmtime(timestamp))

76a102e013fc521d66d70f85e94f8bf8d0b78889
dennisvtang <dennisvtang@email.com>
2023-11-10 02:55:36 +0000

    Update from https://github.com/dennisvtang/obs-stream-markers/commit/459207db670c567d720cd5bf55b1b8e211b69138

--- dep/obs-stream-markers/src/create_stream_markers.py
+++ dep/obs-stream-markers/src/create_stream_markers.py
@@ -2,13 +2,11 @@
 # this script will create/append to a file
 # each line in the file will correspond to the time in the VOD when the hotkey is pressed
 
-from typing import Union
-import time
-import datetime
 import pathlib
 import logging
 # pylint: disable=import-error
 import obspython as obs
+from Timestamps import write_timestamp, get_timestamp
 
 logging.basicConfig(
     level=logging.CRITICAL,
@@ -27,86 +25,6 @@ SCRIPT_SETTINGS = {
     'start_timestamp': None,
     'last_timestamp': None,
 }
-
-
-def write_timestamp(
-    output_folder_path: pathlib.Path,
-    output_type: str,
-    stream_service: str,
-    start_time: str,
-    start_timestamp: float,
-    last_timestamp: str
-):
-    """Writes given timestamp to appropriate VOD marker textfile in given output_folder_path.
-
-    Duplicate timestamps, ie if create stream marker is spammed, will not be created.
-    The stream marker file will have the timestamp of the start of a stream/recording and the
-    service being streamed on.
-    """
-    current_timestamp = get_timestamp('float')
-    assert output_folder_path != pathlib.Path('.'), (
-        'Output folder not set in script settings'
-    )
-    if output_type is None and logging.root.level != logging.INFO:
-        print('No marker saved, not recording or streaming.')
-        return
-    if last_timestamp == convert_timestamp_to_playback_time(current_timestamp):
-        logging.info('Prevented writing duplicate timestamp')
-        return
-    last_timestamp = convert_timestamp_to_playback_time(current_timestamp)
-
-    output_folder_path.mkdir(parents=True, exist_ok=True)
-    filename = f'{start_time} - {output_type or "DEBUG"}'
-    if stream_service is not None:
-        filename += f' - {stream_service}'
-    filename += '.txt'
-    output_file_path = output_folder_path / filename
-
-    logging.info(output_file_path)
-    vod_timestamp_diff = current_timestamp - start_timestamp
-    with output_file_path.open('a', encoding='utf-8') as file:
-        logging.info(f'Adding marker to: {output_file_path}')
-        file.write(
-            f'{convert_timestamp_to_playback_time(vod_timestamp_diff)}\n'
-        )
-
-# todo test other ways of getting python imports working when the script is running in obs
-
-
-def get_timestamp(output_format, date_format='%Y-%m-%d %H-%M-%S'):
-    """Returns the POSIX timestamp.
-
-    Supported output formats are "float" and "string".
-    "float" returns the timestamp as a float.
-    "string" returns the timestamp in the following format: "HOUR-MINUTE-SECONDS", where zeros are
-    always padded
-    """
-    supported_formats = ['float', 'string']
-    assert output_format in supported_formats, f'Choose a supported format, {supported_formats}'
-
-    timestamp = datetime.datetime.now()
-
-    if output_format == 'float':
-        timestamp = timestamp.timestamp()
-    elif output_format == 'string':
-        timestamp = timestamp.strftime(date_format)
-
-    return timestamp
-
-
-def convert_timestamp_to_playback_time(
-    timestamp: Union[float, int],
-    playback_format: str = "%H:%M:%S"
-):
-    """Converts a timestamp (seconds) into playback time (HOUR-MINUTE-SECONDS).
-
-    The returned playback time has padded zeros.
-    """
-    assert isinstance(timestamp, (float, int)), (
-        'timestamp must be a float or int type.'
-    )
-
-    return time.strftime(playback_format, time.gmtime(timestamp))
 
 
 def determine_streaming_service(stream_url):

be02a30538cbe89285bba64744c5a7997cc1ed11
Dennis Tang <dennisvtang@gmail.com>
2024-01-15 17:25:58 -0800

    Remove unused function
    Function was for writing to file locally

--- src/timestamps.py
+++ src/timestamps.py
@@ -1,50 +1,6 @@
 import datetime
-import logging
-import pathlib
 import time
 from typing import Union
-
-
-def write_timestamp(
-    output_folder_path: pathlib.Path,
-    output_type: str,
-    stream_service: str,
-    start_time: str,
-    start_timestamp: float,
-    last_timestamp: str
-):
-    """Writes given timestamp to appropriate VOD marker textfile in given output_folder_path.
-
-    Duplicate timestamps, ie if create stream marker is spammed, will not be created.
-    The stream marker file will have the timestamp of the start of a stream/recording and the
-    service being streamed on.
-    """
-    current_timestamp = get_timestamp('float')
-    assert output_folder_path != pathlib.Path('.'), (
-        'Output folder not set in script settings'
-    )
-    if output_type is None and logging.root.level != logging.INFO:
-        logging.info('No marker saved, not recording or streaming.')
-        return
-    if last_timestamp == convert_timestamp_to_playback_time(current_timestamp):
-        logging.info('Prevented writing duplicate timestamp')
-        return
-    last_timestamp = convert_timestamp_to_playback_time(current_timestamp)
-
-    output_folder_path.mkdir(parents=True, exist_ok=True)
-    filename = f'{start_time} - {output_type or "DEBUG"}'
-    if stream_service is not None:
-        filename += f' - {stream_service}'
-    filename += '.txt'
-    output_file_path = output_folder_path / filename
-
-    logging.info(output_file_path)
-    vod_timestamp_diff = current_timestamp - start_timestamp
-    with output_file_path.open('a', encoding='utf-8') as file:
-        logging.info(f'Adding marker to: {output_file_path}')
-        file.write(
-            f'{convert_timestamp_to_playback_time(vod_timestamp_diff)}\n'
-        )
 
 
 def get_timestamp(output_format, date_format='%Y-%m-%d %H-%M-%S'):

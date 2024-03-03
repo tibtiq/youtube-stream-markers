ebc3036b99f27ee54b15a29dc9a26c3c56c12ed8
Dennis Tang <dennisvtang@gmail.com>
2024-03-02 22:07:24 -0800

    one-line docstring

--- src/create_youtube_stream_markers.py
+++ src/create_youtube_stream_markers.py
@@ -203,8 +203,7 @@ def script_load(settings):
     obs.obs_frontend_add_event_callback(on_event_callback)
 
     def hotkey_callback_args(button_down):
-        """Hack to pass additional arguments to callback function
-        """
+        """Hack to pass additional arguments to callback function."""
         return hotkey_callback(button_down)
     HOTKEY_ID_ARRAY.append(obs.obs_hotkey_register_frontend(
         'SHORTCUT 1',

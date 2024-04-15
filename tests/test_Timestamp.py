90d79bcfa64cdb27f87cd0dddb74cb97a3554d0a
Dennis Tang <dennisvtang@gmail.com>
2024-04-14 17:29:05 -0700

    Create test_Timestamp.py

---
+++ tests/test_Timestamp.py
@@ -0,0 +1,27 @@
+import datetime
+
+import pytest
+
+from src.Timestamp import Timestamp
+
+
+class Test_Timestamp:
+    class Test_add:
+        def test_other_is_timestamp(self):
+            pass
+
+        def test_other_is_int(self):
+            pass
+
+        def test_other_is_float(self):
+            pass
+
+    class Test_sub:
+        def test_other_is_timestamp(self):
+            pass
+
+        def test_other_is_int(self):
+            pass
+
+        def test_other_is_float(self):
+            pass

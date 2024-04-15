aad1fde939cd25090aeb06c3cfbd04ba2d7ff805
Dennis Tang <dennisvtang@gmail.com>
2024-04-14 17:42:39 -0700

    Add tests for __add__

--- tests/test_Timestamp.py
+++ tests/test_Timestamp.py
@@ -8,13 +8,28 @@ from src.Timestamp import Timestamp
 class Test_Timestamp:
     class Test_add:
         def test_other_is_timestamp(self):
-            pass
+            timestamp = Timestamp()
+            adjusted_timestamp = timestamp + 5
+
+            expected_timestamp = datetime.datetime.now() + datetime.timedelta(seconds=5)
+
+            assert adjusted_timestamp.datetime == expected_timestamp
 
         def test_other_is_int(self):
-            pass
+            timestamp = Timestamp()
+            adjusted_timestamp = timestamp + 5
+
+            expected_timestamp = datetime.datetime.now() + datetime.timedelta(seconds=5)
+
+            assert adjusted_timestamp.datetime == expected_timestamp
 
         def test_other_is_float(self):
-            pass
+            timestamp = Timestamp()
+            adjusted_timestamp = timestamp + 5.5
+
+            expected_timestamp = datetime.datetime.now() + datetime.timedelta(seconds=5.5)
+
+            assert adjusted_timestamp.datetime == expected_timestamp
 
     class Test_sub:
         def test_other_is_timestamp(self):

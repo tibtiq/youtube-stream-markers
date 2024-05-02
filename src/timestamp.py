4c2a13bcc183e85c6e896b088c0c5771dd75c7de
Dennis Tang <dennisvtang@gmail.com>
2024-05-02 16:17:23 -0700

    25 refactor timestamp related code into a class (#33)
    * Refactor timestamp related code
    
    * Refactor filename
    
    * Setup test suite
    
    * Forgot to use timestamp_format
    
    * Update .gitignore
    
    * Add eq magic function
    
    * Create test_Timestamp.py
    
    * Refactor variable names
    
    * Add tests cases for __sub__
    
    * Add tests for __add__
    
    * Refactor code to clearly define test values
    
    * add and sub support float type
    
    * typo
    
    * Fix these tests
    
    Doesn't make sense to add two Timestamp objects
    
    * Add test case for add and sub other is None
    
    * test for sub default TypeError
    
    * Grammer
    
    * Create run_tests.ps1
    
    * conform to snakecase
    
    * Add missing test
    
    * Fix run test script
    
    * Use new class
    
    * Remove stub test class
    
    * Setup test for str
    
    * Put expected value on right
    
    * Verify value is str
    
    * Rename function
    
    * remove unused float magic function
    
    * Add test for eq magic function
    
    * Fix name error
    
    * Add test for change_timestamp_format
    
    * Add test for as_playback_time
    
    * Add check for wrong type
    
    * Unnecessary f-string
    
    * Update requirements.txt
    
    * Update requirements.txt
    
    * temp
    
    * Adhere to snake_case
    
    Update create_youtube_stream_markers.py
    
    * Line too long
    
    * Remove completed todo
    
    * Delete requirements.txt
    
    * Create requirements.txt
    
    * Unnecessary parenthesis
    
    * Separate extra detail from one line docstring
    
    * Setup optional typehint
    
    * Grammer
    
    * Empty line after docstring
    
    * Grammer
    
    * Remove old comments

---
+++ src/timestamp.py
@@ -0,0 +1,129 @@
+"""This module contains useful functions related to manipulating datetime objects."""
+
+from __future__ import annotations
+
+import datetime
+import time
+from typing import Optional, Union
+
+
+class Timestamp:
+    """Wrapper class for datetime.datetime objects.
+
+    This class made with the goal of working with youtube timestamps.
+    """
+
+    def __init__(
+        self,
+        timestamp_time: Optional[datetime.datetime] = None,
+        timestamp_format: str = '%H:%M:%S'
+    ) -> None:
+        """Create a Timestamp class with an underlying datetime.datetime object of now.
+
+        Args:
+            timestamp_format: str, optional ['%H:%M:%S']
+                Timestamp format to use when convert Timestamp into a str. Default value uses
+                padded zeros. For example:
+                01:23:45
+                20:03:45
+            timestamp_time: str [None]
+                Used to create a Timestamp object with a specific time.
+
+        """
+        if isinstance(timestamp_time, datetime.datetime):
+            self.datetime = timestamp_time
+        else:
+            self.datetime = datetime.datetime.now()
+        self.timestamp_format = timestamp_format
+
+    def __str__(self) -> str:
+        """Return str representation of timestamp."""
+        return self.datetime.strftime(self.timestamp_format)
+
+    def __add__(self, other) -> Union[int, Timestamp]:
+        """Overloads the '+' operator for addition.
+
+        Args:
+            other: Timestamp
+                The object added to this instance.
+
+        Returns:
+            An int representing the combined total seconds of other and this instance.
+            A new Timestamp object with the result of the addition.
+        """
+        if isinstance(other, (int, float)):
+            time_diff = datetime.timedelta(seconds=other)
+            adjusted_timestamp = self.datetime + time_diff
+            return Timestamp(adjusted_timestamp)
+
+        raise TypeError(
+            f"unsupported operand type(s) for +: '{type(self)}' and '{type(other)}'"
+        )
+
+    def __sub__(self, other) -> Union[int, Timestamp]:
+        """Overloads the '-' operator for subtraction.
+
+        Args:
+            other: Timestamp
+                The object subtract from this instance.
+
+        Returns:
+            An int representing the total seconds difference between other and this instance.
+            A new Timestamp object with the result of the subtract.
+        """
+        if isinstance(other, Timestamp):
+            return round((self.datetime - other.datetime).total_seconds())
+        if isinstance(other, (int, float)):
+            time_diff = datetime.timedelta(seconds=other)
+            adjusted_timestamp = self.datetime - time_diff
+            return Timestamp(adjusted_timestamp)
+        if other is None:
+            return 0
+
+        raise TypeError(
+            f"unsupported operand type(s) for -: '{type(self)}' and '{type(other)}'"
+        )
+
+    def __eq__(self, other: object) -> bool:
+        """Overloads the '==' operator for equality comparison.
+
+        Args:
+            other: Timestamp or compatible object
+                The object to compare for equality with this instance.
+
+        Returns:
+            bool: True if the objects are considered equal, False otherwise.
+        """
+        if isinstance(other, Timestamp):
+            return self.datetime == other.datetime
+        if isinstance(other, datetime.datetime):
+            return self.datetime == other
+
+        return False
+
+    def change_timestamp_format(self, new_format: str) -> None:
+        """Change timestamp format used when converting Timestamp into string.
+
+        Args:
+            new_format: str
+                New format to replace current timestamp_format.
+        """
+        self.timestamp_format = new_format
+
+    def as_playback_time(self, start_time: Timestamp) -> str:
+        """Convert Timestamp to playback time (HOUR-MINUTE-SECONDS) given a starting Timestamp.
+
+        The returned playback time has padded zeros.
+
+        Args:
+            playback_time: str
+                Timestamp converted to playback time given a starting Timestamp.
+        """
+        if not isinstance(start_time, Timestamp):
+            raise TypeError("Value passed to 'start_time' isn't a Timestamp object")
+
+        return time.strftime(
+            self.timestamp_format, time.gmtime(
+                (self.datetime - start_time.datetime).total_seconds()
+            )
+        )

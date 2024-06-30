b424d9b69ec512117d05552a95e8c013b399a3e4
tibtiq <29826331+tibtiq@users.noreply.github.com>
2024-06-30 14:11:07 -0700

    These tests should only test the asserts

--- tests/test_youtube_interface.py
+++ tests/test_youtube_interface.py
@@ -7,21 +7,11 @@ from src.youtube_interface import get_youtube_credentials_from_oauth
 
 class Test_get_youtube_credentials_from_oauth:
     class Test_asserts:
-        def test_path_exists(self, tmp_path):
-            path = tmp_path / 'file.json'
-            open(path, 'w', encoding='utf-8')
-
-            with pytest.raises(SystemExit):
-                get_youtube_credentials_from_oauth(path)
-
         def test_path_doesnt_exist(self, tmp_path):
             path = tmp_path / 'file.json'
 
             with pytest.raises(Exception):
                 get_youtube_credentials_from_oauth(path)
-
-        def test_path_file_type_correct(self):
-            pass
 
         def test_path_file_type_incorrect(self):
             path = pathlib.Path()

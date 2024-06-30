a76dd2d5275ed7a90ec20ce4ef9165c6b7114675
tibtiq <29826331+tibtiq@users.noreply.github.com>
2024-06-30 13:53:07 -0700

    add test case

--- tests/test_youtube_interface.py
+++ tests/test_youtube_interface.py
@@ -13,11 +13,11 @@ class Test_get_youtube_credentials_from_oauth:
 
         #     get_youtube_credentials_from_oauth(path)
 
-        # def test_path_doesnt_exist(self, tmp_path):
-        #     path = tmp_path / 'file.json'
+        def test_path_doesnt_exist(self, tmp_path):
+            path = tmp_path / 'file.json'
 
-        #     with pytest.raises(Exception):
-        #         get_youtube_credentials_from_oauth(path)
+            with pytest.raises(Exception):
+                get_youtube_credentials_from_oauth(path)
 
         def test_path_file_type_correct(self):
             pass

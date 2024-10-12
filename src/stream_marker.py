2bd80fcf54ecaae4b46002cc6879aa497a3aecce
tibtiq <29826331+tibtiq@users.noreply.github.com>
2024-10-12 16:45:19 -0700

    Update comments

--- src/stream_marker.py
+++ src/stream_marker.py
@@ -10,7 +10,7 @@ from typing import Optional, Union
 class StreamMarker:
     """Wrapper class for datetime.datetime objects.
 
-    This class made with the goal of working with youtube timestamps.
+    This class made with the goal of working with youtube stream markers.
     """
 
     def __init__(
@@ -18,16 +18,16 @@ class StreamMarker:
         stream_marker_time: Optional[datetime.datetime] = None,
         stream_marker_format: str = '%H:%M:%S'
     ) -> None:
-        """Create a Timestamp class with an underlying datetime.datetime object of now.
+        """Create a StreamMarker class with an underlying datetime.datetime object of now.
 
         Args:
             stream_marker_format: str, optional ['%H:%M:%S']
-                Timestamp format to use when convert Timestamp into a str. Default value uses
+                Timestamp format to use when convert StreamMarker into a str. Default value uses
                 padded zeros. For example:
                 01:23:45
                 20:03:45
             stream_marker_time: str [None]
-                Used to create a Timestamp object with a specific time.
+                Used to create a StreamMarker object with a specific time.
 
         """
         if isinstance(stream_marker_time, datetime.datetime):
@@ -37,19 +37,19 @@ class StreamMarker:
         self.stream_marker_format = stream_marker_format
 
     def __str__(self) -> str:
-        """Return str representation of timestamp."""
+        """Return str representation of StreamMarker."""
         return self.datetime.strftime(self.stream_marker_format)
 
     def __add__(self, other) -> Union[int, StreamMarker]:
         """Overloads the '+' operator for addition.
 
         Args:
-            other: Timestamp
+            other: StreamMarker
                 The object added to this instance.
 
         Returns:
             An int representing the combined total seconds of other and this instance.
-            A new Timestamp object with the result of the addition.
+            A new StreamMarker object with the result of the addition.
         """
         if isinstance(other, (int, float)):
             time_diff = datetime.timedelta(seconds=other)
@@ -64,12 +64,12 @@ class StreamMarker:
         """Overloads the '-' operator for subtraction.
 
         Args:
-            other: Timestamp
+            other: StreamMarker
                 The object subtract from this instance.
 
         Returns:
             An int representing the total seconds difference between other and this instance.
-            A new Timestamp object with the result of the subtract.
+            A new StreamMarker object with the result of the subtract.
         """
         if isinstance(other, StreamMarker):
             return round((self.datetime - other.datetime).total_seconds())
@@ -88,7 +88,7 @@ class StreamMarker:
         """Overloads the '==' operator for equality comparison.
 
         Args:
-            other: Timestamp or compatible object
+            other: StreamMarker or compatible object
                 The object to compare for equality with this instance.
 
         Returns:
@@ -102,7 +102,7 @@ class StreamMarker:
         return False
 
     def change_stream_marker_format(self, new_format: str) -> None:
-        """Change timestamp format used when converting Timestamp into string.
+        """Change StreamMarker format used when converting StreamMarker into string.
 
         Args:
             new_format: str
@@ -111,16 +111,16 @@ class StreamMarker:
         self.stream_marker_format = new_format
 
     def as_playback_time(self, start_time: StreamMarker) -> str:
-        """Convert Timestamp to playback time (HOUR-MINUTE-SECONDS) given a starting Timestamp.
+        """Convert StreamMarker to playback time (HOUR-MINUTE-SECONDS) given a starting StreamMarker.
 
         The returned playback time has padded zeros.
 
         Args:
             playback_time: str
-                Timestamp converted to playback time given a starting Timestamp.
+                StreamMarker converted to playback time given a starting StreamMarker.
         """
         if not isinstance(start_time, StreamMarker):
-            raise TypeError("Value passed to 'start_time' isn't a Timestamp object")
+            raise TypeError("Value passed to 'start_time' isn't a StreamMarker object")
 
         return time.strftime(
             self.stream_marker_format, time.gmtime(

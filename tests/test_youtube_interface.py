301df32e2e6342b503ac45e6f87df5fb5d932dde
tibtiq <29826331+tibtiq@users.noreply.github.com>
2024-10-13 13:00:37 -0700

    Update existing test to test assert

--- tests/test_youtube_interface.py
+++ tests/test_youtube_interface.py
@@ -33,13 +33,19 @@ class Test_get_youtube_credentials_from_oauth:
     class Test_asserts:
         def test_path_doesnt_exist(self, tmp_path):
             path = tmp_path / 'file.json'
+            expected_assert_message = "The provided path for api credentials doesn't exist"
 
-            with pytest.raises(Exception):
+            with pytest.raises(AssertionError) as info:
                 get_youtube_credentials_from_oauth(path)
+            assert expected_assert_message in str(info.value)
 
         def test_path_file_type_incorrect(self, tmp_path):
             path = tmp_path / 'file.txt'
             open(path, 'w', encoding='utf-8')
+            expected_assert_message = (
+                "The provided path for api credentials appears to be incorrect file type."
+            )
 
-            with pytest.raises(Exception):
+            with pytest.raises(AssertionError) as info:
                 get_youtube_credentials_from_oauth(path)
+            assert expected_assert_message in str(info.value)

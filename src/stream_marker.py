747b8bb948e4d3adbd794a27be4ee6b1f40f604f
tibtiq <29826331+tibtiq@users.noreply.github.com>
2025-01-11 09:30:30 -0800

    Update example to use StreamMarker

--- src/stream_marker.py
+++ src/stream_marker.py
@@ -104,7 +104,7 @@ class StreamMarker:
 
         Example:
             stream_marker = StreamMarker(2025-01-11 17:00:10.000000)
-            stream_marker.as_playback_time(StreamMarker(2025-01-11 17:00:00.000000))
+            stream_marker.as_playback_time(stream_marker + 5)
             will return
             00:00:10
 

d24300690d9a001f92c27bf25fb5e137d6faed0d
tibtiq <29826331+tibtiq@users.noreply.github.com>
2026-08-01 18:22:10 +0000

    leverage tmp_path in test
    this too
    
    this too

--- tests/test_youtube_interface.py
+++ tests/test_youtube_interface.py
@@ -19,15 +19,16 @@ class Test_get_youtube_credentials_from_file:
             assert expected_assert_message in str(info.value)
 
         def test_path_file_type_incorrect(self, tmp_path):
-            path = tmp_path / "file.txt"
-            open(path, "w", encoding="utf-8")
+            temp_file = tmp_path / "file.txt"
+            temp_file.write_text("Hello World", encoding="utf-8")
+
             expected_assert_message = (
                 "The provided path for api credentials appears to be incorrect file type. "
                 'Expected file type is "dat"'
             )
 
             with pytest.raises(AssertionError) as info:
-                get_youtube_credentials_from_file(path)
+                get_youtube_credentials_from_file(temp_file)
             assert expected_assert_message in str(info.value)
 
 
@@ -44,10 +45,10 @@ class Test_get_youtube_credentials_from_oauth:
             assert expected_assert_message in str(info.value)
 
         def test_path_file_type_incorrect(self, tmp_path):
-            path = tmp_path / "file.txt"
-            open(path, "w", encoding="utf-8")
+            temp_file = tmp_path / "file.txt"
+            temp_file.write_text("Hello World", encoding="utf-8")
             expected_assert_message = "The provided path for api credentials appears to be incorrect file type."
 
             with pytest.raises(AssertionError) as info:
-                get_youtube_credentials_from_oauth(path)
+                get_youtube_credentials_from_oauth(temp_file)
             assert expected_assert_message in str(info.value)
